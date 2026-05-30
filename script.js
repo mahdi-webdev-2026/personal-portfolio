f2a0c51cfba1f2a3da06b6ce948b6e10bc05e897
mahdi-webdev-2026 <mahdikarami09011796236@gmail.com>
2026-05-30 20:44:01 +0200

    Improve portfolio design and animations

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Portfolio Loaded");
+console.log("Portfolio Loaded"); 
+const typingElement =
+    document.getElementById("typing-text");
+
+const roles = [
+    "Future Fachinformatiker",
+    "Web Developer",
+    "IT Enthusiast",
+    "Problem Solver"
+];
+
+let roleIndex = 0;
+
+setInterval(() => {
+
+    roleIndex++;
+
+    if(roleIndex >= roles.length){
+        roleIndex = 0;
+    }
+
+    typingElement.textContent =
+        roles[roleIndex];
+
+}, 2500);
